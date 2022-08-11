af4d5025f80706d8e1d984175a1b7e72cb8814c0
MonsterPtn <hophantennhan@gmail.com>
2022-08-11 09:43:59 +0700

    -AppBarWidget

--- lib/Widget/app_bar_widget.dart
+++ lib/Widget/app_bar_widget.dart
@@ -1,9 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:ui/themes/app_colors.dart';
 import 'package:ui/themes/style_text.dart';
 
 class AppBarWidget extends StatefulWidget implements PreferredSizeWidget {
@@ -29,7 +25,6 @@ class AppBarWidget extends StatefulWidget implements PreferredSizeWidget {
   State<AppBarWidget> createState() => _AppBarWidgetState();
 
   @override
-  // TODO: implement preferredSize
   Size get preferredSize => AppBar().preferredSize;
 }
 
@@ -42,8 +37,9 @@ class _AppBarWidgetState extends State<AppBarWidget> {
       elevation: 0,
       title: Text(
         widget.title ?? '',
-        style: StylesText.styleHeader
-            .copyWith(color: widget.colorTitle ?? Colors.black),
+        style: StylesText.styleHeader.copyWith(
+          color: widget.colorTitle ?? Colors.black,
+        ),
       ),
       leading: widget.isShowBack
           ? InkWell(
@@ -68,9 +64,10 @@ class _AppBarWidgetState extends State<AppBarWidget> {
         widget.isShowMainAction
             ? InkWell(
                 child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: SvgPicture.asset('assets/svg/ic_arrow_left.svg'),
-              ))
+                  padding: const EdgeInsets.all(15.0),
+                  child: SvgPicture.asset('assets/svg/ic_arrow_left.svg'),
+                ),
+              )
             : const SizedBox(),
       ],
     );
