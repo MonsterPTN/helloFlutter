5b531c9390786665585cd2d26b62a148d936f437
MonsterPtn <hophantennhan@gmail.com>
2022-08-11 10:42:16 +0700

    -AddNewModel

---
+++ lib/models/station.dart
@@ -0,0 +1,21 @@
+class Station {
+  int id;
+  String status;
+  String price;
+  String classification;
+  String assessor;
+  String customer;
+  String numberPhoneCustomer;
+  String vehicleDetailStart;
+  String vehicleDetailEnd;
+  Station(
+      this.id,
+      this.status,
+      this.price,
+      this.classification,
+      this.customer,
+      this.assessor,
+      this.numberPhoneCustomer,
+      this.vehicleDetailStart,
+      this.vehicleDetailEnd);
+}
