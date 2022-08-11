46f3e8b493573f45408527dad34b51e659cebcf1
MonsterPtn <hophantennhan@gmail.com>
2022-08-11 09:01:37 +0700

    -LoginScreen

--- lib/modules/LoginScreen/login_screen.dart
+++ lib/modules/LoginScreen/login_screen.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:ui/Widget/text_field_widget.dart';
-import 'package:ui/modules/NewAppraisal/new_appraisal.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:ui/modules/PickerScreen/picker_screen.dart';
-
-import 'package:ui/similar_models.dart';
 import 'package:ui/themes/app_colors.dart';
 import 'package:ui/themes/style_text.dart';
 
 import '../../Widget/button_add_widget.dart';
-import '../../Widget/button_navigation.dart';
-import '../ButtonScreen/button_screen.dart';
 import '../NavigationBottom/bottom_navigator.dart';
-import '../PricingScreen/pricing_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -28,74 +21,61 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Container(
         color: AppColors.backgroundApp,
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               _widgetTextSignIn(text: 'Sign In'),
               const SizedBox(
                 height: 50,
               ),
-              Row(
-                children: const [
-                  Text(
-                    'User ID',
-                    style: StylesText.mediumBoldText,
-                  ),
-                ],
+              const Text(
+                'User ID',
+                style: StylesText.mediumBoldText,
               ),
               const TextFieldWidget(textInput: 'Your user ID here'),
               const SizedBox(
                 height: 30,
               ),
-              Row(
-                children: const [
-                  Text(
-                    'Password',
-                    style: StylesText.mediumBoldText,
-                  ),
-                ],
+              const Text(
+                'Password',
+                style: StylesText.mediumBoldText,
               ),
               const TextFieldWidget(textInput: 'Your password here'),
               const SizedBox(
                 height: 30,
               ),
-              _textForgotPass(text: 'Forgot password'),
+              _textForgotPass('Forgot password'),
               const SizedBox(
                 height: 40,
               ),
               Row(
                 children: [
                   Expanded(
-                    flex: 5,
                     child: ButtonAdd(
                       text: 'SIGN IN',
                       color: AppColors.primari,
+                      colorText: AppColors.backgroundApp,
                       onPressed1: () {
                         Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const BottomNavigatorScreen()));
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const BottomNavigatorScreen(),
+                          ),
+                        );
                       },
                     ),
                   ),
-                  Expanded(
-                      flex: 1,
-                      child: Row(
-                        children: [
-                          const SizedBox(
-                            width: 20,
-                          ),
-                          SvgPicture.asset('assets/svg/ic_find_id.svg'),
-                        ],
-                      )),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  SvgPicture.asset('assets/svg/ic_find_id.svg'),
                 ],
               ),
-              const SizedBox(
-                height: 26,
-              ),
-              _widgetButonNavigation(),
             ],
           ),
         ),
@@ -103,60 +83,19 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget _widgetButonNavigation() {
-    return Wrap(
-      children: [
-        ButtonNavigation(
-          text: 'SimilarModels',
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SimilarModels()),
-            );
-          },
-        ),
-        ButtonNavigation(
-          text: 'PricingScreen',
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const PricingScreen()),
-            );
-          },
-        ),
-        ButtonNavigation(
-          text: 'DemoWidget',
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const PickerScreen()),
-            );
-          },
-        ),
-        ButtonNavigation(
-          text: 'new appraisal',
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const NewAppraisalScreen()),
-            );
-          },
-        ),
-      ],
-    );
-  }
-
   Widget _widgetTextSignIn({required String text}) {
     return Center(
       child: Text(
         text,
-        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 40),
+        style: const TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 40,
+        ),
       ),
     );
   }
 
-  Widget _textForgotPass({required String text}) {
+  Widget _textForgotPass(String text) {
     return Container(
       alignment: Alignment.topLeft,
       child: Text(
