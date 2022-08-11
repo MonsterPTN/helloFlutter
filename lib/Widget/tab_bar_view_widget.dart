1e15edfdab24808dfe492609cbac9de7abcd331f
MonsterPtn <hophantennhan@gmail.com>
2022-08-11 09:24:22 +0700

    -TabBarViewWidget

---
+++ lib/Widget/tab_bar_view_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class TabBarViewWidget extends StatefulWidget {
+  const TabBarViewWidget({Key? key}) : super(key: key);
+
+  @override
+  State<TabBarViewWidget> createState() => _TabBarViewWidgetState();
+}
+
+class _TabBarViewWidgetState extends State<TabBarViewWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [],
+            ),
+            title: const Text('Woolha.com Flutter Tutorial'),
+            backgroundColor: Colors.teal,
+          ),
+          body: const TabBarView(
+            physics: BouncingScrollPhysics(),
+            dragStartBehavior: DragStartBehavior.down,
+            children: [
+              // Add widgets here
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
