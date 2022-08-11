6c52cace18a5418e6f1b6787da16167b82ca8e72
MonsterPtn <hophantennhan@gmail.com>
2022-08-11 09:24:34 +0700

    -TextFieldWidget

--- lib/Widget/text_field_widget.dart
+++ lib/Widget/text_field_widget.dart
@@ -4,9 +4,14 @@ import 'package:flutter_svg/flutter_svg.dart';
 class TextFieldWidget extends StatelessWidget {
   final String textInput;
   final int? lengMax;
+  final String? textRight;
   final SvgPicture? iconRight;
   const TextFieldWidget(
-      {Key? key, required this.textInput, this.lengMax, this.iconRight})
+      {Key? key,
+      required this.textInput,
+      this.lengMax,
+      this.iconRight,
+      this.textRight})
       : super(key: key);
 
   @override
@@ -18,6 +23,7 @@ class TextFieldWidget extends StatelessWidget {
           padding: const EdgeInsets.all(12.0),
           child: iconRight,
         ),
+        suffixText: textRight,
         hintText: textInput,
         counterText: '',
       ),
